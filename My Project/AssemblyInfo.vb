f555bfbcb16c5fc7110b23e8b2bcf3f75d1448c3
angellinares <angel@monzon8.es>
2012-08-21 14:35:10 +0200

    Initial push

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports Rhino.PlugIns
+
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("Custom Sprite")> 
+<Assembly: AssemblyDescription("Custom sprite component for Grasshopper.")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Custom Sprite")> 
+<Assembly: AssemblyCopyright("Copyright © BlurryPaths 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b30ed406-a108-419a-9174-bc14b2488e4a")> ' This will also be the Guid of the Rhino plug-in
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.1")> 
+<Assembly: AssemblyFileVersion("0.0.0.1")> 
