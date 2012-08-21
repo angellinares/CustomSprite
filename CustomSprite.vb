3ae03cc0075346744f00cbd8b72b1d5a64201ecf
angellinares <angel@monzon8.es>
2012-08-21 20:04:06 +0200

    First build -> Didn't work
    Soe problems with list references.

--- CustomSprite.vb
+++ CustomSprite.vb
@@ -3,6 +3,8 @@
 Imports Grasshopper.Kernel
 Imports Rhino.Geometry
 Imports Rhino.Display
+Imports System.Drawing
+Imports Grasshopper
 
 
 Public Class CustomSprite
@@ -17,7 +19,7 @@ Public Class CustomSprite
     Public Sub New()
         MyBase.New("Custom Sprite", "CustomSprite", _
            "Creates a customizable oGL sprite visualization ", _
-           "Extra", "viz")
+           "Vector", "Point")
     End Sub
 
     ''' <summary>
@@ -25,7 +27,8 @@ Public Class CustomSprite
     ''' </summary>
     Protected Overrides Sub RegisterInputParams(ByVal pManager As GH_Component.GH_InputParamManager)
         pManager.Register_StringParam("Sprite", "S", "Sprite file route")
-        pManager.Register_PointParam("Location", "L", "Sprite location")
+        pManager.Register_ColourParam("Color", "C", "Sprite color", GH_ParamAccess.list)
+        pManager.Register_PointParam("Location", "L", "Sprite location", GH_ParamAccess.list)
         pManager.Register_DoubleParam("Size", "S", "Sprite size")
     End Sub
 
@@ -36,15 +39,19 @@ Public Class CustomSprite
     End Sub
 
     Private customSprite As DisplayBitmap
-    Private clusters As DisplayBitmapDrawList
+    Private clusters As List(Of DisplayBitmapDrawList)
     Private size As List(Of Double)
     Private imagePath As String
 
 
-
-
     Public Overrides Sub DrawViewportWires(ByVal args As Grasshopper.Kernel.IGH_PreviewArgs)
         MyBase.DrawViewportWires(args)
+
+        customSprite = New DisplayBitmap(Image.FromFile(imagePath))
+
+        For i As Integer = 0 To clusters.Count - 1
+            args.Display.DrawSprites(customSprite, clusters(i), size(i), True)
+        Next
     End Sub
 
 
@@ -55,6 +62,38 @@ Public Class CustomSprite
     ''' to store data in output parameters.</param>
     ''' 
     Protected Overrides Sub SolveInstance(ByVal DA As IGH_DataAccess)
+        Dim ptlist As New List(Of Point3d)
+        Dim colors As New List(Of Color)
+        Dim dSize As Double
+
+
+        If (DA.Iteration = 0) Then
+            clusters = New List(Of DisplayBitmapDrawList)
+            size = New List(Of Double)
+        End If
+
+        If (Not DA.GetData(Of String)(0, imagePath)) Then Return
+        If (Not DA.GetDataList(Of Point3d)(2, ptlist)) Then Return
+        If (Not DA.GetDataList(Of Color)(1, colors)) Then Return
+
+        If (Not DA.GetData(Of Double)(3, dSize)) Then Return
+
+        If colors.Count <> ptlist.Count Then
+            If (colors.Count = 1) Then
+                Do Until (ptlist.Count = colors.Count)
+                    colors.Add(colors.Item(0))
+                Loop
+            Else
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Different number of points and colors supplied")
+            End If
+        End If
+
+        If ptlist.Count <> 0 Then
+            Dim disp As New DisplayBitmapDrawList
+            disp.SetPoints(ptlist, colors)
+            clusters.Add(disp)
+            size.Add(dSize)
+        End If
 
     End Sub
 
@@ -77,7 +116,7 @@ Public Class CustomSprite
     ''' </summary>
     Public Overrides ReadOnly Property ComponentGuid() As Guid
         Get
-            Return New Guid("{3423ba03-26ad-48f9-8022-510559b26342}")
+            Return New Guid("{AEBE070F-10C2-44E7-8C9A-D543B90DEEBD}")
         End Get
     End Property
 End Class
